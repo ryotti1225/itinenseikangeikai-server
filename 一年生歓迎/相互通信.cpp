de26ebd3a104be94b86ea41334469efb7e6ad730
ryotti1225 <ryosei.o.91803@gmail.com>
2025-04-14 18:16:29 +0900

    テスト

--- 一年生歓迎/相互通信.cpp
+++ 一年生歓迎/相互通信.cpp
@@ -1,85 +1,50 @@
 ﻿#include <boost/asio.hpp>
 #include <iostream>
-#include <thread>
-#include <unordered_set>
 #include <memory>
-#include<deque>
-#include<string>
 
 using boost::asio::ip::tcp;
-using namespace std;
 
-class ChatSession : public enable_shared_from_this<ChatSession> {
+class Session : public std::enable_shared_from_this<Session> {
 public:
-    ChatSession(tcp::socket socket, unordered_set<shared_ptr<ChatSession>>& sessions)
-        : socket_(move(socket)), sessions_(sessions) {
-    }
+    Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
     void start() {
-        sessions_.insert(shared_from_this());
         do_read();
-    }
-
-    void deliver(const string& message) {
-        auto self(shared_from_this());
-        boost::asio::post(socket_.get_executor(), [this, self, message]() {
-            bool write_in_progress = !write_msgs_.empty();
-            write_msgs_.push_back(message);
-            if (!write_in_progress) {
-                do_write();
-            }
-            });
     }
 
 private:
     void do_read() {
         auto self(shared_from_this());
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
-            [this, self](boost::system::error_code ec, size_t length) {
+        socket_.async_read_some(
+            boost::asio::buffer(data_),
+            [this, self](boost::system::error_code ec, std::size_t length) {
                 if (!ec) {
-                    string message(data_, length);
-                    broadcast(message);
+                    std::cout << "Received: " << std::string(data_, length) << std::endl;
+                    do_write(length);
+                }
+            });
+    }
+
+    void do_write(std::size_t length) {
+        auto self(shared_from_this());
+        boost::asio::async_write(
+            socket_,
+            boost::asio::buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
                     do_read();
                 }
-                else {
-                    sessions_.erase(shared_from_this());
-                }
             });
-    }
-
-    void do_write() {
-        auto self(shared_from_this());
-        boost::asio::async_write(socket_, boost::asio::buffer(write_msgs_.front()),
-            [this, self](boost::system::error_code ec, size_t /*length*/) {
-                if (!ec) {
-                    write_msgs_.pop_front();
-                    if (!write_msgs_.empty()) {
-                        do_write();
-                    }
-                }
-                else {
-                    sessions_.erase(shared_from_this());
-                }
-            });
-    }
-
-    void broadcast(const string& message) {
-        for (auto& session : sessions_) {
-            session->deliver(message);
-        }
     }
 
     tcp::socket socket_;
-    unordered_set<shared_ptr<ChatSession>>& sessions_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-    deque<string> write_msgs_;
+    char data_[1024];
 };
 
-class ChatServer {
+class Server {
 public:
-    ChatServer(boost::asio::io_context& io_context, const tcp::endpoint& endpoint)
-        : acceptor_(io_context, endpoint) {
+    Server(boost::asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
         do_accept();
     }
 
@@ -88,25 +53,24 @@ private:
         acceptor_.async_accept(
             [this](boost::system::error_code ec, tcp::socket socket) {
                 if (!ec) {
-                    make_shared<ChatSession>(move(socket), sessions_)->start();
+                    std::make_shared<Session>(std::move(socket))->start();
                 }
                 do_accept();
             });
     }
 
     tcp::acceptor acceptor_;
-    unordered_set<shared_ptr<ChatSession>> sessions_;
 };
 
 int main() {
     try {
         boost::asio::io_context io_context;
-        tcp::endpoint endpoint(tcp::v4(), 12345);
-        ChatServer server(io_context, endpoint);
+        Server server(io_context, 12345);
+
+        std::cout << "Server is running on port 12345..." << std::endl;
         io_context.run();
-    }
-    catch (exception& e) {
-        cerr << "Exception: " << e.what() << "\n";
+    } catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
     return 0;
